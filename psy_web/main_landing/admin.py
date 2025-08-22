fc0ee52e4ff80a8047c6f01ed7896cfbf93a5b87
Egor Ivanov <rust_lang_lover@proton.me>
2025-08-22 18:27:34 +0300

    // set up for further connections

--- psy_web/main_landing/admin.py
+++ psy_web/main_landing/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import (
+    NameBlock, EducationPiece,
+    ServicesBlock, ServicesPiece,
+    CertificatesBlock,
+    MyExperienceBlock, FactsPiece
+)
 
-# Register your models here.
+# --- NameBlock и связанные образования ---
+class EducationPieceInline(admin.TabularInline):
+    model = EducationPiece
+    extra = 1  # сколько пустых форм показывать для добавления новых записей
+
+@admin.register(NameBlock)
+class NameBlockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'my_quote', 'hours')
+    inlines = [EducationPieceInline]
+
+# --- ServicesBlock и связанные услуги ---
+class ServicesPieceInline(admin.TabularInline):
+    model = ServicesPiece
+    extra = 1
+
+@admin.register(ServicesBlock)
+class ServicesBlockAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    inlines = [ServicesPieceInline]
+
+# --- CertificatesBlock ---
+@admin.register(CertificatesBlock)
+class CertificatesBlockAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timing', 'online_pricing', 'actual_pricing')
+
+# --- MyExperienceBlock и связанные факты ---
+class FactsPieceInline(admin.TabularInline):
+    model = FactsPiece
+    extra = 1
+
+@admin.register(MyExperienceBlock)
+class MyExperienceBlockAdmin(admin.ModelAdmin):
+    list_display = ('hours_with_clients', 'hours_of_studying', 'my_quote')
+    inlines = [FactsPieceInline]
