1b32a189104308361e21ac6ee3326e1ade7ec573
Egor Ivanov <rust_lang_lover@proton.me>
2025-08-25 16:59:56 +0300

    // blog is fully working. Needs styling

--- psy_web/a_blog/models.py
+++ psy_web/a_blog/models.py
@@ -12,6 +12,11 @@ class BlogPage(Page):
         FieldPanel('body'),
     ]
     template = "a_blog/blog_page.html"
+    def get_context(self, request):
+        articles = self.get_children().live().order_by('-first_published_at')
+        context = super().get_context(request)
+        context['articles'] = articles
+        return context
 
 class ArticlePage(Page):
     intro = models.CharField(max_length=80)
