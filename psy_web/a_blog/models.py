72ad66fb9e4f32df6a5302dcf76ed56a12153a0b
Egor Ivanov <rust_lang_lover@proton.me>
2025-08-25 15:46:09 +0300

    // чекпоинт??

--- psy_web/a_blog/models.py
+++ psy_web/a_blog/models.py
@@ -1,6 +1,8 @@
+from datetime import date
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Page
+from django.db import models
 
 
 # Create your models here.
@@ -10,3 +12,22 @@ class BlogPage(Page):
         FieldPanel('body'),
     ]
     template = "a_blog/blog_page.html"
+
+class ArticlePage(Page):
+    intro = models.CharField(max_length=80)
+    body = RichTextField(blank=True)
+    date = models.DateField("Post date", default=date.today)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=80)
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('image'),
+        FieldPanel('caption'),
+        FieldPanel('body'),
+        FieldPanel('date'),
+    ]
