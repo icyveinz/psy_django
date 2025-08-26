1529ca4a246bfbe81f22dcb9b1b50bee798d1329
Egor Ivanov <rust_lang_lover@proton.me>
2025-08-26 20:33:14 +0300

    // фикс галп

--- psy_web/frontend/gulpfile.js
+++ psy_web/frontend/gulpfile.js
@@ -1,10 +1,8 @@
-const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin');
 
 gulp.task('styles', function () {
     return gulp.src('sass/**/*.sass') // or 'sass/**/*.scss' if you're using SCSS
@@ -12,8 +10,7 @@ gulp.task('styles', function () {
         .pipe(concat('main.css')) // combine all into one file
         .pipe(cleanCSS()) // minify the CSS
         .pipe(rename({ suffix: '.min' })) // add .min suffix: main.min.css
-        .pipe(gulp.dest(path.resolve(__dirname, './static/css'))); // output to static folder
+        .pipe(gulp.dest('../static/css')); // output to static folder
 });
 
-
-gulp.task('default', gulp.parallel('styles'));
+gulp.task('default', gulp.series('styles'));
