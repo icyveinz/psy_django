c9fb98b2d06b1ff5cff36944e377295ae128fdfd
Egor Ivanov <rust_lang_lover@proton.me>
2025-09-17 18:13:14 +0300

    sitemaps.xml теперь работает

---
+++ psy_web/psy_web/seo_tools.py
@@ -0,0 +1,36 @@
+from django.contrib.sitemaps import Sitemap
+from django.http import HttpResponse
+from wagtail.models import Page
+
+def robots_txt(request):
+    content = """
+User-agent: *
+Disallow: /admin/
+Disallow: /login/
+Disallow: /cms/
+Disallow: /wagtail_landing/
+Allow: /
+
+Sitemap: https://example.com/sitemap.xml
+"""
+    return HttpResponse(content, content_type="text/plain")
+
+
+class StaticPageSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.5
+
+    def items(self):
+        return Page.objects.live().public()
+
+    def location(self, obj):
+        url = obj.get_url()
+        return url if url else '/'
+
+    def lastmod(self, obj):
+        return obj.latest_revision_created_at
+
+
+sitemaps = {
+    'pages': StaticPageSitemap,
+}
--- psy_web/psy_web/urls.py
+++ psy_web/psy_web/urls.py
@@ -1,31 +1,21 @@
 from django.contrib import admin
-from django.http import HttpResponse
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from psy_web.seo_tools import robots_txt, sitemaps
+
 
 def trigger_error(request):
     division_by_zero = 1 / 0
-
-def robots_txt(request):
-    content = """
-User-agent: *
-Disallow: /admin/
-Disallow: /login/
-Disallow: /cms/
-Disallow: /wagtail_landing/
-Allow: /
-
-Sitemap: https://example.com/sitemap.xml
-"""
-    return HttpResponse(content, content_type="text/plain")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('wagtail_landing/', include('wagtail_landing.urls')),
     path('robots.txt', robots_txt),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('sentry-debug/', trigger_error),
     path('', include(wagtail_urls)),
 ]
