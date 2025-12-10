482a9d6512fa06f682cf2ae714f562ac925de8d2
Egor Ivanov <rust_lang_lover@proton.me>
2025-12-10 15:11:43 +0300

    обновил настройки безопасности в settings.py

--- psy_web/psy_web/settings.py
+++ psy_web/psy_web/settings.py
@@ -127,9 +127,9 @@ WAGTAILDOCS_EXTENSIONS = [
 # Security settings for production
 if not DEBUG:
     # HTTPS enforcement !закомментировать если нужно поработать без дебага!
-    # SECURE_SSL_REDIRECT = True
-    # SESSION_COOKIE_SECURE = True
-    # CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
 
     # HSTS (HTTP Strict Transport Security)
     SECURE_HSTS_SECONDS = 31536000  # 1 year
